29d4c399c7c187a6c1a6aed2d0364b99b6864826
quinhos <marco.castagnaro@ing.austral.edu.ar>
2024-05-31 14:36:49 -0300

    CORRECT SCRIPT vol4

--- deployDev.sh
+++ deployDev.sh
@@ -3,9 +3,10 @@
 SSH_USER="tricolor-ingsis"
 SSH_HOST="4.203.104.146"
 REPO_PATH="ingSis-microservices"
-GIT_USERNAME="tricolor-austral"
+GIT_USERNAME="marcocastagnaro"
 GIT_TOKEN="GIT_TOKEN"
 DEVM_KEY="DEVM_KEY"
+GIT_GROUP= "tricolor-austral"
 
 echo "${DEVM_KEY}" > dev_key.pem
 
@@ -17,7 +18,7 @@ chmod 400 dev_key.pem
 
 ssh -t -i dev_key.pem tricolor-ingsis@4.203.104.146 << EOF
   cd ${REPO_PATH}
-  sudo git pull https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${GIT_USERNAME}/${REPO_PATH}.git
+  sudo git pull https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${GIT_GROUP}/${REPO_PATH}.git
   sudo docker-compose -f docker-compose.dev.yml up --build
 EOF
 rm dev_key.pem
