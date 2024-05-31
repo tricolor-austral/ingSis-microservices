eb71f3ecc1d1f42fa3e29eba9cfe7acc82ae5db8
quinhos <marco.castagnaro@ing.austral.edu.ar>
2024-05-31 15:09:04 -0300

    CORRECT SCRIPT vol6

--- deployDev.sh
+++ deployDev.sh
@@ -5,15 +5,15 @@ GIT_USERNAME="marcocastagnaro"
 GIT_TOKEN=git_token
 GIT_GROUP= "tricolor-austral"
 
-echo dev_key.pem
-
 chmod 400 dev_key.pem
 cat dev_key.pem
-cat GIT_TOKEN
+cat git_token
+cat pull https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${GIT_GROUP}/${REPO_PATH}.git
 
 ssh -t -i dev_key.pem tricolor-ingsis@4.203.104.146 << EOF
   cd ${REPO_PATH}
-  sudo git pull https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/${GIT_GROUP}/${REPO_PATH}.git
+  sudo git pull https://${GIT_USERNAME}:git_token@github.com/${GIT_GROUP}/${REPO_PATH}.git
   sudo docker-compose -f docker-compose.dev.yml up --build
 EOF
 rm dev_key.pem
+
